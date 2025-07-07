2b60cc8e6eab0eeab4760e9aa65a7c98aeb83930
Patryk Kups <pkups@anaconda.com>
2025-07-08 00:00:53 +0200

    test

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,5 @@
-set AWS_C_INSTALL=%LIBRARY_PREFIX%
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+copy %LIBRARY_LIB%\\libcrypto.lib %LIBRARY_LIB%\crypto.lib
+if errorlevel 1 exit 1
 
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+if errorlevel 1 exit 1
