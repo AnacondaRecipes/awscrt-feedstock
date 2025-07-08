b63b525acff36b5d16355448482d62f5c791a32a
PatrykKups <pkups@anaconda.com>
2025-07-08 15:42:48 +0200

    awscrt 0.27.4  (#3)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,6 @@
-set AWS_C_INSTALL=%LIBRARY_PREFIX%
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+@REM LINK : fatal error LNK1181: cannot open input file 'crypto.lib'
+copy %LIBRARY_LIB%\\libcrypto.lib %LIBRARY_LIB%\crypto.lib
+if errorlevel 1 exit 1
 
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,4 @@
 
 set -exuo pipefail
 
-# Remove bundled dependencies
-rm -rf crt
-
-export AWS_C_INSTALL=$PREFIX
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
